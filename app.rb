f44737a9ecce047c99b556de626ccb1eb734cf6a
Taylor Collins <taylorcc33@gmail.com>
2020-11-12 11:15:59 -0700

    workflow updates

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ menu_test = Menu.new
 
 puts menu_test.class
   
+# generally have message like 'please select an option'
+# list of options displayed
+# user selects an option
+
+# Please Select an option
+# 1) Option 1
+# 2) Option 2
+# 3) EXIT
+
+# what kind of data structure does that look like? Hash
+
+menu = {
+  message: "Please select an option",
+  options: ['add contact', 'update contact', 'EXIT']
+}
